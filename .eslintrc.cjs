4134ed3856dc3b76030ae1fe0fbd9ae6cc78f0de
Dinh-Van Colomban <van.colomban@gmail.com>
2022-06-05 18:04:12 +0200

    chore(build): fix pnpm lock file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,5 +3,5 @@ module.exports = {
   plugins: ['@dvcol/presets'],
   extends: ['plugin:@dvcol/presets/vue', 'plugin:@dvcol/presets/jest', 'plugin:@dvcol/presets/vitest'],
   // to apply jest linting even-though we use vitest
-  settings: { jest: { version: 27 } },
+  settings: { jest: { version: 27 } }
 };
