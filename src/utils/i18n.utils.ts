02036afb5dfdc96bd83506b2c0cd14a478e255c1
Dinh-Van Colomban <van.colomban@gmail.com>
2023-08-26 22:22:48 +0200

    fix(web): fix local url fetching

--- src/utils/i18n.utils.ts
+++ src/utils/i18n.utils.ts
@@ -3,15 +3,19 @@ import { useI18n as chromeUseI18n } from '@dvcol/web-extension-utils/lib/chrome/
 import type { Locale } from '~/models/i18n.model';
 
 import { useI18nStore } from '~/stores/i18n.store';
+import { useRouterStore } from '~/stores/router.store';
 
 export const useI18n = (...roots: string[]): ReturnType<typeof chromeUseI18n> => {
   if (!chrome?.i18n) {
     const store = useI18nStore();
+    const router = useRouterStore();
 
-    fetch(new URL(`./_locales/${store.lang}/messages.json`, new URL(import.meta.url).origin))
-      .then(r => r.json())
-      .then((locale: Locale) => store.addLocale(locale))
-      .catch(err => console.error(`Failed to fetch locale '${store.lang}'`, err));
+    if (!store.locales?.[store.lang]) {
+      fetch(new URL(`${router.baseName ?? '.'}/_locales/${store.lang}/messages.json`, new URL(import.meta.url).origin))
+        .then(r => r.json())
+        .then((locale: Locale) => store.addLocale(locale))
+        .catch(err => console.error(`Failed to fetch locale '${store.lang}'`, err));
+    }
 
     return (value, modules) => store.i18n(value, ...(modules?.length ? modules : roots));
   }
