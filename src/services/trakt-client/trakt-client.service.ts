857e9e0fca758f807d2507e7637cc1a49bb3c5fe
Dinh-Van Colomban <van.colomban@gmail.com>
2024-02-17 00:59:40 +0100

    feat(cache): create chrome storage cache

--- src/services/trakt-client/trakt-client.service.ts
+++ src/services/trakt-client/trakt-client.service.ts
@@ -1,4 +1,9 @@
+import type { TraktApiResponse } from '~/models/trakt/trakt-client.model';
+
+import { traktApi } from '~/services/trakt-client/api/trakt-api.endpoints';
 import { TraktClient } from '~/services/trakt-client/clients/trakt-client';
 import { traktClientSettings } from '~/settings/traktv.api';
+import { ChromeCacheStore } from '~/utils/cache.utils';
 
-export const traktService = new TraktClient(traktClientSettings);
+const cacheStore = new ChromeCacheStore<TraktApiResponse>({ prefix: 'trakt-cache' });
+export const traktService = new TraktClient({ ...traktClientSettings, cacheStore }, {}, traktApi);
--- src/services/tvdb-client/tvdb-client.service.ts
+++ src/services/tvdb-client/tvdb-client.service.ts
@@ -1,4 +1,8 @@
+import type { TvdbApiResponse } from '~/models/tvdb/tvdb-client.model';
+
 import { TvdbClient } from '~/services/tvdb-client/clients/tvdb-client';
 import { tvdbClientSettings } from '~/settings/tvdb.api';
+import { ChromeCacheStore } from '~/utils/cache.utils';
 
-export const tvdbService = new TvdbClient(tvdbClientSettings);
+const cacheStore = new ChromeCacheStore<TvdbApiResponse>({ prefix: 'tmdb-cache' });
+export const tvdbService = new TvdbClient({ ...tvdbClientSettings, cacheStore });
