44e18e0d8dc22132c97479d70758a0bc8175e175
Dinh-Van Colomban <van.colomban@gmail.com>
2024-02-17 18:19:31 +0100

    chore(devtool): adds devtool support to wc

--- src/services/tmdb-client/tmdb-client.service.ts
+++
@@ -1,8 +0,0 @@
-import type { TmdbApiResponse } from '~/models/tmdb/tmdb-client.model';
-
-import { TmdbClient } from '~/services/tmdb-client/clients/tmdb-client';
-import { tmdbClientSettings } from '~/settings/tmdb.api';
-import { ChromeCacheStore } from '~/utils/cache.utils';
-
-const cacheStore = new ChromeCacheStore<TmdbApiResponse>({ prefix: 'tmdb-cache' });
-export const tmdbService = new TmdbClient({ ...tmdbClientSettings, cacheStore });
--- src/web/create-wc.ts
+++ src/web/create-wc.ts
@@ -1,5 +1,6 @@
 import { defineCustomElement, getCurrentInstance, h } from 'vue';
 
+import type { DevToolsHook, DevToolsHooks } from '@vue/devtools';
 import type { Component, ComponentInternalInstance } from 'vue';
 import type { InitVueAppOption } from '~/web/init-vue-app';
 
@@ -7,7 +8,13 @@ import { initVueApp } from '~/web/init-vue-app';
 
 type ComponentInstance = ComponentInternalInstance & { provides: ComponentInternalInstance['appContext']['provides'] };
 
-export const createElementInstance = (component: Component, options: InitVueAppOption) => {
+declare global {
+  interface Window {
+    __VUE_DEVTOOLS_GLOBAL_HOOK__: DevToolsHook & { Vue: Component };
+  }
+}
+
+export const createElementInstance = (component: Component, { name, ...options }: InitVueAppOption & { name: string }) => {
   return defineCustomElement({
     setup(props) {
       const app = initVueApp(component, options);
@@ -16,6 +23,24 @@ export const createElementInstance = (component: Component, options: InitVueAppO
       if (inst) {
         Object.assign(inst.appContext, app._context);
         Object.assign(inst.provides, app._context.provides);
+
+        // Add support for Vue Devtools
+        if (import.meta.env.DEV && window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
+          app._container = document.querySelector(name);
+          app._instance = inst;
+
+          const types = {
+            Comment: Symbol('v-cmt'),
+            Fragment: Symbol('v-fgt'),
+            Static: Symbol('v-stc'),
+            Text: Symbol('v-txt'),
+          };
+
+          window.__VUE_DEVTOOLS_GLOBAL_HOOK__.emit('app:init' as DevToolsHooks, app, app.version, types);
+          window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app;
+
+          console.info('Vue Devtools is enabled.');
+        }
       }
 
       return () => h(component, props);
