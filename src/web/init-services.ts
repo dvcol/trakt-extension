a459f4a93c54672eadd6f918a908012adae36e7d
Dinh-Van Colomban <van.colomban@gmail.com>
2024-02-20 01:57:55 +0100

    chore: remove log

--- src/web/init-services.ts
+++ src/web/init-services.ts
@@ -8,8 +8,6 @@ export const initServices = async () => {
   const { syncRestoreAuth } = useAuthSettingsStore();
   const { syncRestoreUser, syncRestoreAllUsers } = useUserSettingsStore();
 
-  console.info('init-services', window.location.href);
-
   const restoredSettings = await syncRestoreUser();
   const restoredAuth = await syncRestoreAuth(restoredSettings?.user?.username);
 
