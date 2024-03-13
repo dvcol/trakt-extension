1b3f1f41198c65e053088f86208f8d1c507876bb
Dinh-Van Colomban <van.colomban@gmail.com>
2024-03-13 01:21:55 +0100

    chore(calendar): creates routes for calendar navbar view

--- src/components/views/calendar/CalendarComponent.vue
+++ src/components/views/calendar/CalendarComponent.vue
@@ -1,9 +1,61 @@
 <script lang="ts" setup>
 // TODO
+import { NTimeline, NTimelineItem } from 'naive-ui';
 </script>
 
 <template>
-  <span>This is a calendar component</span>
+  <NTimeline size="large">
+    <NTimelineItem content="Oops" />
+    <NTimelineItem
+      type="success"
+      title="Success"
+      content="success content"
+      time="2018-04-03 20:46"
+    />
+    <NTimelineItem type="error" content="Error content" time="2018-04-03 20:46" />
+    <NTimelineItem
+      type="warning"
+      title="Warning"
+      content="warning content"
+      time="2018-04-03 20:46"
+    />
+    <NTimelineItem
+      type="info"
+      title="Info"
+      content="info content"
+      time="2018-04-03 20:46"
+    />
+    <NTimelineItem
+      type="info"
+      title="Info"
+      content="info content"
+      time="2018-04-03 20:46"
+    />
+    <NTimelineItem
+      type="info"
+      title="Info"
+      content="info content"
+      time="2018-04-03 20:46"
+    />
+    <NTimelineItem
+      type="info"
+      title="Info"
+      content="info content"
+      time="2018-04-03 20:46"
+    />
+    <NTimelineItem
+      type="info"
+      title="Info"
+      content="info content"
+      time="2018-04-03 20:46"
+    />
+    <NTimelineItem
+      type="info"
+      title="Info"
+      content="info content"
+      time="2018-04-03 20:46"
+    />
+  </NTimeline>
 </template>
 
 <style lang="scss" scoped>
