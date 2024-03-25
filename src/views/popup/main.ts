b9bc72b0c6dbf9820d36c0433f03d2b091979481
Dinh-Van Colomban <van.colomban@gmail.com>
2024-03-25 20:13:58 +0100

    fix(ui): auto-resize based on window inner width/height

--- src/views/popup/main.ts
+++ src/views/popup/main.ts
@@ -1,5 +1,12 @@
 import defineComponent from '~/web/define-component';
 
+const root = document?.getElementById('app-popup');
+
+if (root) {
+  root.style.width = `${window.innerWidth}px`;
+  root.style.height = `${window.innerHeight}px`;
+}
+
 defineComponent({ baseUrl: import.meta.env.VITE_BASE })
   .then(() => console.info('Web Component defined'))
   .catch(err => console.error('Failed to define component', err));
