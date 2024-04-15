002b7b948da9b9c497b05d4da4f6b5ec3247d059
Dinh-Van Colomban <van.colomban@gmail.com>
2024-04-15 18:20:04 +0200

    fix(test): fix logging issues for i18n

--- src/utils/i18n.utils.ts
+++ src/utils/i18n.utils.ts
@@ -2,7 +2,6 @@ import type { Locale, Locales } from '~/models/i18n.model';
 
 import { useI18nStore } from '~/stores/i18n.store';
 import { useRouterStore } from '~/stores/router.store';
-import { logger } from '~/stores/settings/log.store';
 import { useI18nTranslate } from '~/utils/browser/browser-i18n.utils';
 import { chromeI18n } from '~/utils/browser/browser.utils';
 
@@ -11,7 +10,7 @@ export const initLocalI18n = () => {
   const router = useRouterStore();
   let promise: Promise<Locales | void> | undefined;
   if (import.meta.hot) {
-    logger.debug('Listening to i18n HMR changes');
+    console.debug('Listening to i18n HMR changes');
     import.meta.hot.send('fetch:i18n');
     import.meta.hot.on('update:i18n', (data: { lang: string; locale: Locale }[]) => {
       data?.forEach(({ lang, locale }) => store.addLocale(locale, lang, true));
@@ -20,7 +19,7 @@ export const initLocalI18n = () => {
     promise = fetch(new URL(`${router.baseUrl ?? './'}_locales/${store.lang}/messages.json`, new URL(import.meta.url).origin))
       .then(r => r.json())
       .then((locale: Locale) => store.addLocale(locale))
-      .catch(err => logger.error(`Failed to fetch locale '${store.lang}'`, err));
+      .catch(err => console.error(`Failed to fetch locale '${store.lang}'`, err));
   }
 
   return { store, router, promise };
