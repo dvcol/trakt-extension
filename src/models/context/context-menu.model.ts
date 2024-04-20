44bb741199543901233cbb1b85df496df11be810
Dinh-Van Colomban <van.colomban@gmail.com>
2024-04-20 21:07:30 +0200

    feat(context): adds a context menu to add to search history only

--- src/models/context/context-menu.model.ts
+++ src/models/context/context-menu.model.ts
@@ -1,5 +1,6 @@
 export const ContextMenuId = {
   OpenInSideTrakt: 'open-in-side-trakt',
+  AddToSearchHistory: 'add-to-search-history',
 } as const;
 
 export type ContextMenuIds = (typeof ContextMenuId)[keyof typeof ContextMenuId];
@@ -14,6 +15,11 @@ export const ContextMenus: Record<ContextMenuIds, ContextMenu> = {
   [ContextMenuId.OpenInSideTrakt]: {
     id: ContextMenuId.OpenInSideTrakt,
     title: 'Open in side trakt',
+    contexts: ['selection'],
+  },
+  [ContextMenuId.AddToSearchHistory]: {
+    id: ContextMenuId.AddToSearchHistory,
+    title: 'Add to search history',
     contexts: ['selection'],
   },
 } as const;
