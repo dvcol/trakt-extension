3c47116de47535fa24a9bb4944d0e575a2c77a77
Dinh-Van Colomban <van.colomban@gmail.com>
2024-07-15 23:35:26 +0200

    feat(pwa): adds basic service worker

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import { fileURLToPath, URL } from 'url';
 
 import vue from '@vitejs/plugin-vue';
 import MagicString from 'magic-string';
-
 import { viteVueCE } from 'unplugin-vue-ce';
 
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import dtsPlugin from 'vite-plugin-dts';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import pkg from './package.json';
 import { isDev, port, resolveParent } from './scripts/utils';
@@ -39,88 +39,121 @@ const getInput = (hmr: boolean, _isWeb: boolean): InputOption => {
 
 const i18nRegex = /.*src\/i18n\/([a-zA-Z]+)\/.*\.json/;
 
-const getPlugins = (): PluginOption[] => [
-  dtsPlugin({
-    include: ['index.ts', 'web/**'],
-    outDir: resolveParent('dist/lib'),
-  }),
-  vue({
-    customElement: true,
-  }),
-  viteVueCE({}),
-  checker({
-    vueTsc: {
-      tsconfigPath: 'tsconfig.app.json',
-    },
-  }),
-  {
-    name: 'i18n-hmr',
-    configureServer: server => {
-      console.info('server start');
-      server.ws.on('fetch:i18n', async () => {
-        const dir = await readdir('dist/_locales');
-        const locales = dir.map(_lang =>
-          readFile(`dist/_locales/${_lang}/messages.json`, { encoding: 'utf-8' }).then(locale => ({ lang: _lang, locale: JSON.parse(locale) })),
-        );
-        server.ws.send({
-          type: 'custom',
-          event: 'update:i18n',
-          data: await Promise.all(locales),
+const getPlugins = (_isDev: boolean, _isWeb: boolean): PluginOption[] => {
+  const plugins: PluginOption[] = [
+    dtsPlugin({
+      include: ['index.ts', 'web/**'],
+      outDir: resolveParent('dist/lib'),
+    }),
+    vue({
+      customElement: true,
+    }),
+    viteVueCE({}),
+    checker({
+      vueTsc: {
+        tsconfigPath: 'tsconfig.app.json',
+      },
+    }),
+    {
+      name: 'i18n-hmr',
+      configureServer: server => {
+        console.info('server start');
+        server.ws.on('fetch:i18n', async () => {
+          const dir = await readdir('dist/_locales');
+          const locales = dir.map(_lang =>
+            readFile(`dist/_locales/${_lang}/messages.json`, { encoding: 'utf-8' }).then(locale => ({ lang: _lang, locale: JSON.parse(locale) })),
+          );
+          server.ws.send({
+            type: 'custom',
+            event: 'update:i18n',
+            data: await Promise.all(locales),
+          });
         });
-      });
+      },
+      handleHotUpdate: async ({ server, file, read, modules }) => {
+        const lang = file.match(i18nRegex)?.[1];
+        if (lang) {
+          console.info('Emit new i18n', file);
+          const locale = JSON.parse(await read());
+          server.ws.send({
+            type: 'custom',
+            event: 'update:i18n',
+            data: [{ lang, locale }],
+          });
+        }
+        return modules;
+      },
     },
-    handleHotUpdate: async ({ server, file, read, modules }) => {
-      const lang = file.match(i18nRegex)?.[1];
-      if (lang) {
-        console.info('Emit new i18n', file);
-        const locale = JSON.parse(await read());
-        server.ws.send({
-          type: 'custom',
-          event: 'update:i18n',
-          data: [{ lang, locale }],
-        });
-      }
-      return modules;
-    },
-  },
 
-  // replace document query selector to inject naive-ui within shadow root instead of document head
-  {
-    name: 'chunk-transform',
-    enforce: 'post',
-    transform: (code, id) => {
-      if (id.includes('css-render') || id.includes('naive-ui')) {
-        const getReplacement = (fallback = 'document') => `(document.querySelector('wc-trakt-extension').shadowRoot ?? ${fallback})`;
-        const _code = new MagicString(code)
-          .replace(/document\.querySelector/g, `(${getReplacement()}).querySelector`)
-          .replace(/document\.head/g, `(${getReplacement('document.head')})`);
-        return {
-          code: _code.toString(),
-          map: _code.generateMap({ hires: true }) as ExistingRawSourceMap,
-        };
-      }
+    // replace document query selector to inject naive-ui within shadow root instead of document head
+    {
+      name: 'chunk-transform',
+      enforce: 'post',
+      transform: (code, id) => {
+        if (id.includes('css-render') || id.includes('naive-ui')) {
+          const getReplacement = (fallback = 'document') => `(document.querySelector('wc-trakt-extension').shadowRoot ?? ${fallback})`;
+          const _code = new MagicString(code)
+            .replace(/document\.querySelector/g, `(${getReplacement()}).querySelector`)
+            .replace(/document\.head/g, `(${getReplacement('document.head')})`);
+          return {
+            code: _code.toString(),
+            map: _code.generateMap({ hires: true }) as ExistingRawSourceMap,
+          };
+        }
+      },
     },
-  },
 
-  // rewrite assets to use relative path
-  {
-    name: 'assets-rewrite',
-    enforce: 'post',
-    apply: 'build',
-    transformIndexHtml: (html, { path }) => html.replace(/"\/assets\//g, `"${relative(dirname(path), '/assets').replace(/\\/g, '/')}/`),
-  },
-
-  {
-    name: 'write-to-disk',
-    apply: 'serve',
-    handleHotUpdate: async ({ file, server: { config } }) => {
-      const srcDir = dirname(file);
-      if (!srcDir?.endsWith('src/scripts/background')) return;
-      const outPath = `${join(config.build.outDir, 'scripts/background')}.js`;
-      await writeFile(outPath, `import 'http://localhost:3303/scripts/background/index.ts';`);
+    // rewrite assets to use relative path
+    {
+      name: 'assets-rewrite',
+      enforce: 'post',
+      apply: 'build',
+      transformIndexHtml: (html, { path }) => html.replace(/"\/assets\//g, `"${relative(dirname(path), '/assets').replace(/\\/g, '/')}/`),
     },
-  },
-];
+
+    {
+      name: 'write-to-disk',
+      apply: 'serve',
+      handleHotUpdate: async ({ file, server: { config } }) => {
+        const srcDir = dirname(file);
+        if (!srcDir?.endsWith('src/scripts/background')) return;
+        const outPath = `${join(config.build.outDir, 'scripts/background')}.js`;
+        await writeFile(outPath, `import 'http://localhost:3303/scripts/background/index.ts';`);
+      },
+    },
+  ];
+
+  if (!_isDev && _isWeb) {
+    plugins.push(
+      VitePWA({
+        scope: '/trakt-extension/',
+        registerType: 'prompt',
+        includeAssets: ['**/favicon.ico', '**/*.svg', '**/*.png', '**/*.webp', '**/*.json'],
+        manifest: {
+          name: 'Side Trakt',
+          short_name: 'side-trakt',
+          description: 'Side Trakt (companion app for Trakt.tv)',
+          theme_color: '#80000a',
+          background_color: '#ffffff',
+          display: 'standalone',
+          icons: [
+            {
+              src: 'icons/icon-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+        workbox: {
+          sourcemap: true,
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,json,mp4}'],
+        },
+      }),
+    );
+  }
+
+  return plugins;
+};
 
 export default defineConfig(() => ({
   root: resolveParent('src'),
@@ -137,7 +170,7 @@ export default defineConfig(() => ({
     'import.meta.env.PKG_VERSION': JSON.stringify(pkg.version),
     'import.meta.env.PKG_NAME': JSON.stringify(pkg.name),
   },
-  plugins: getPlugins(),
+  plugins: getPlugins(isDev, isWeb),
   base: './',
   server: {
     port,
