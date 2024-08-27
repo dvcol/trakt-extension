36838bc45b615747d14df4b47290daf457eb30f0
Dinh-Van Colomban <van.colomban@gmail.com>
2024-08-27 13:03:34 +0200

    chore(shell): adds loading indicator to shell skeleton

--- src/components/common/loading/PageLoading.vue
+++ src/components/common/loading/PageLoading.vue
@@ -5,17 +5,20 @@ import IconLoadingMatrix from '~/components/icons/IconLoadingMatrix.vue';
 </script>
 
 <template>
-  <NIcon class="loading-container" size="6rem">
+  <NIcon class="loading-container" size="3rem">
     <IconLoadingMatrix />
   </NIcon>
 </template>
 
 <style lang="scss" scoped>
+@use '~/styles/layout' as layout;
+
 .loading-container {
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
+  padding-bottom: layout.$header-navbar-height;
 }
 </style>
